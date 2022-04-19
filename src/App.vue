222c7f13dffde89113c82f18a3ee047167d75c9a
weigangyi <1315230417@qq.com>
2022-04-20 00:17:51 +0800

    登录信息的数据交互

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,10 @@
 </template>
 
 <script>
-
 export default {
   name: 'app',
 };
 </script>
 
 <style>
-
 </style>
---
+++ src/views/layout/components/sliderNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main-header">
+    <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <!-- 面包屑 -->
+    <div class="breadcrumb">
+      <a-breadcrumb>
+        <a-breadcrumb-item>首页</a-breadcrumb-item>
+        <a-breadcrumb-item>
+          <a href>统计</a>
+        </a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <!-- 用户信息 -->
+    <ul class="user-info">
+      <li>
+        欢迎**
+        <a-icon type="down" />
+      </li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
+
+<style>
+</style>
