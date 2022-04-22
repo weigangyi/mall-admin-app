b226624e756836c91347b74eaea4f93454b679c8
weigangyi <1315230417@qq.com>
2022-04-22 23:42:34 +0800

    菜单栏权限配置

---
+++ src/utils/permission.js
@@ -0,0 +1,34 @@
+// 存储的是 角色对应的权限名称
+const roleToRoute = {
+  coustomer: [{
+    name: 'Product',
+  }, {
+    name: 'ProductList',
+  }, {
+    name: 'ProductAdd',
+  }],
+  admin: [{
+    name: 'Product',
+  }, {
+    name: 'ProductList',
+  }, {
+    name: 'ProductAdd',
+  }, {
+    name: 'Category',
+  }],
+};
+
+// 获取 过滤掉没有权限的路由
+export default function getMenuRoute(role, routes) {
+  const allowRoutesName = roleToRoute[role].map((item) => item.name);
+  const resultRoutes = routes.filter((r) => {
+    const obj = r;
+    if (allowRoutesName.indexOf(r.name) !== -1) {
+      const { children } = obj;
+      obj.children = children.filter((c) => allowRoutesName.indexOf(c.name) !== -1);
+      return true;
+    }
+    return false;
+  });
+  return resultRoutes;
+}
---
+++ src/views/page/productAdd.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>新增商品</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
