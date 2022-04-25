7db2ee497aec3cba8595e965e09853583121a272
weigangyi <1315230417@qq.com>
2022-04-25 22:58:58 +0800

    菜单栏过滤显示

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ const ayncRouterMap = [{ // 异步路由数组
   name: 'Product',
   meta: {
     title: '商品',
+    icon: 'inbox',
+    hidden: false,
   },
   component: Home,
   children: [{
@@ -18,6 +20,8 @@ const ayncRouterMap = [{ // 异步路由数组
     name: 'ProductList',
     meta: {
       title: '商品列表',
+      icon: 'unordered-list',
+      hidden: false,
     },
     component: () => import('@/views/page/productList.vue'),
   }, {
@@ -25,6 +29,8 @@ const ayncRouterMap = [{ // 异步路由数组
     name: 'ProductAdd',
     meta: {
       title: '添加商品',
+      icon: 'file-add',
+      hidden: false,
     },
     component: () => import('@/views/page/productAdd.vue'),
   }, {
@@ -32,6 +38,8 @@ const ayncRouterMap = [{ // 异步路由数组
     name: 'category',
     meta: {
       title: '类目管理',
+      icon: 'project',
+      hidden: false,
     },
     component: () => import('@/views/page/category.vue'),
   }],
@@ -44,12 +52,16 @@ const routes = [
     component: Home,
     meta: {
       title: '首页',
+      icon: 'home',
+      hidden: false,
     },
     children: [{
       path: 'Index',
       name: 'Index',
       meta: {
         title: '统计',
+        icon: 'number',
+        hidden: false,
       },
       // 懒加载
       component: () => import('@/views/page/index.vue'),
@@ -61,6 +73,7 @@ const routes = [
     component: Login,
     meta: {
       title: '登录',
+      hidden: true,
     },
   },
 
@@ -76,8 +89,10 @@ router.beforeEach((to, from, next) => {
     if (store.state.user.appkey && store.state.user.username && store.state.user.role) {
       if (!isAddRoutes) {
         const menuRoutes = getMenuRoutes(store.state.user.role, ayncRouterMap);
-        router.addRoutes(menuRoutes);
-        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes));
+        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes)).then(() => {
+          router.addRoutes(menuRoutes);
+          next();
+        });
         isAddRoutes = true;
       }
       return next();
